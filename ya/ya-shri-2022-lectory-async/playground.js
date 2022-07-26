298ef3e57d21bcc8a5fa57491fd7440c6d142f39
Koutsenko Dmitry <koutsenko@gmail.com>
2022-07-27 01:05:49 +0300

    First solution

--- ya/ya-shri-2022-lectory-async/playground.js
+++ ya/ya-shri-2022-lectory-async/playground.js
@@ -61,11 +61,56 @@ solution(input).then(result => {
 });
 
 async function solution(input) {
-    // ... решение задачи
+    console.time('solution');
+
+    // Создаем робкие и настойчивые реализации методов для массива через промисы.  
+
+    const getSize = (input) => new Promise((resolve, reject) => {
+        input.size((size) => size === null ? reject() : resolve(size));
+    });
+
+    const getElem = (input, index) => new Promise((resolve, reject) => {
+        input.read(index, (elem) => elem === null ? reject() : resolve(elem));
+    });
+
+    const getEnsuredSize = (input) => new Promise((resolve) => {
+        input.size((size) => resolve(size === null ? getEnsuredSize(input) : size));
+    });
+
+    const getEnsuredElem = (input, index) => new Promise((resolve) => {
+        input.read(index, (elem) => resolve(elem === null ? getEnsuredElem(input, index) : elem));
+    });
 
     // пример вызова read
-    input.read(0, (elem) => console.log(`read: ${elem}`));
+    // input.read(0, (elem) => console.log(`read: ${elem}`));
 
     // пример вызова size
-    input.size((size) => console.log(`size: ${size}`));
+    // input.size((size) => console.log(`size: ${size}`));
+
+    /**
+     * Рекурсивный обход массива.
+     */
+    const walk = async (asyncArray, acc = []) => {
+        const size = await getEnsuredSize(asyncArray);
+        if (size === 0) {
+            return acc;
+        }
+        
+        const promises = [];
+        for (let i = 0; i < size; i++) {
+            const elem = await getEnsuredElem(asyncArray, i);
+            if (elem instanceof AsyncArray) {
+                const elemAcc = await walk(elem, acc);
+                promises.push(...elemAcc);
+            } else {
+                promises.push(elem);
+            }
+        }
+        
+        return await Promise.all(promises);
+    }
+
+    const result = await walk(input);
+    console.timeEnd('solution');
+    return result;
 }
