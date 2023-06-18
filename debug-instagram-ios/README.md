8d9eea3a9e2ce05293c938ebe906d7b20c63588c
Koutsenko Dmitry <koutsenko@gmail.com>
2023-06-18 22:41:48 +0300

    Cleanup. Added simple node example

--- debug-instagram-ios/README.md
+++
@@ -1,3 +0,0 @@
-### Проверка работы SPA внутри Instagram для iOS
-
-Ссылка: http://koutsenko.github.io/debug-instagram-ios
---
+++ nodejs/non-blocking-readfile.js
@@ -0,0 +1,20 @@
+﻿/**
+ * Демонстрация асинхронного выполнения кода.
+ *
+ * Код выведет:
+ * "Начало чтения файла"
+ * "Конец работы"
+ * "Ошибка чтения".
+ */
+
+const fs = require("node:fs");
+
+console.log("Начало чтения файла");
+fs.readFile("file.md", "utf-8", function (err, content) {
+  if (err) {
+    console.log("Ошибка чтения");
+    return console.log(err);
+  }
+  console.log(content);
+});
+console.log("Конец работы");
