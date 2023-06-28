6c6654cf5be2a230a81b77f527f7dd8948e43d09
Koutsenko Dmitry <koutsenko@gmail.com>
2023-06-28 17:39:29 +0300

    Currencies sum

---
+++ sum_currency.js
@@ -0,0 +1,5 @@
+﻿const amounts = ["0.20", "45.90", "13.21"];
+const numbers = amounts.map((a) => parseFloat(a));
+const sum = numbers.reduce((acc, cur) => parseFloat((acc + cur).toFixed(2)), 0);
+
+console.log(sum);
